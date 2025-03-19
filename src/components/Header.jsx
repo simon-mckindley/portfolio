e7116f86e42d484a386f783e4677ebe2a1994323
SimonMck789 <S9406133@student.rmit.edu.au>
2025-03-19 23:57:55 +1100

    added heading animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ export default function Header() {
     return (
         <header>
             <h1>Simon McKindley</h1>
-            <h2 className="sub-head">Full Stack Developer</h2>
+            <div className="sub-head">
+                <h2 className="upper">Full Stack Developer</h2>
+                <h2 className="lower">Software Engineer</h2>
+            </div>
         </header>
     )
 }
